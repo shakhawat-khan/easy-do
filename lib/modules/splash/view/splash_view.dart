c0844d54ab39933828197cb2d2f388183fa94894
shakhawat-khan <satanfromhelll666@gmail.com>
2024-01-24 22:20:30 +0600

    add splash screen

---
+++ lib/modules/splash/view/splash_view.dart
@@ -0,0 +1,55 @@
+import 'package:easy_do/modules/home_screen/view/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
+        return const HomeScreen();
+      }));
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/splash_color.png'),
+          ),
+        ),
+        child: const Center(
+          child: Image(
+            image: AssetImage(
+              'assets/images/splash_logo.png',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
